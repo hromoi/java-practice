c9a4308611bf25504640b539c4e9e75b3fd99c4d
Savinov Evgeny <Zsavinoww@gmail.com>
2020-01-27 11:58:45 +0300

    update refactoring

--- repository/javaio/JavaIOSkillRepositoryImpl.java
+++ repository/javaio/JavaIOSkillRepositoryImpl.java
@@ -10,33 +10,27 @@ import java.util.List;
 public class JavaIOSkillRepositoryImpl implements SkillRepository {
 
     public Skill getById(Long id){
-        Skill skill = new Skill();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("/Users/user/IdeaProjects/first_crud_application/src/skills.txt")))){
             String[] array;
             String str;
             for (int i = 0; i <= id; i++) {
                 if ((str = reader.readLine()) != null && i == id){
-                    array = str.split(",+");
-                    skill.setId(Long.valueOf(array[0]));
-                    skill.setName(array[1]);
-                    break ;
+                    return parse(str);
                 }
             }
         } catch (IOException e){
             System.out.println("error i/o");
         }
-        return skill;
+        return null;
     }
 
     public List<Skill> getAll(){
         List<Skill> myList = new ArrayList<>();;
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("/Users/user/IdeaProjects/first_crud_application/src/skills.txt")))){
             String str;
-            String[] array;
             do {
                 if ((str = reader.readLine()) != null){
-                    array = str.split(",+");
-                    myList.add(new Skill(Long.valueOf(array[0]), array[1]));
+                    myList.add(parse(str));
                 }
             } while (str != null);
         } catch (IOException e){
@@ -60,11 +54,9 @@ public class JavaIOSkillRepositoryImpl implements SkillRepository {
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("/Users/user/IdeaProjects/first_crud_application/src/skills.txt")))){
             String str = null;
-            String[] array;
             do{
                 if ((str = reader.readLine()) != null){
-                    array = str.split(",+");
-                    myList.add(new Skill(Long.valueOf(array[0]), array[1]));
+                    myList.add(parse(str));
                 }
             } while (str != null);
         } catch (IOException e){
@@ -92,7 +84,7 @@ public class JavaIOSkillRepositoryImpl implements SkillRepository {
             String str;
             do {
                 if ((str = reader.readLine()) != null){
-                   myList.add(new Skill(Long.valueOf(str.split(",+")[0]), str.split(",+")[1]));
+                   myList.add(parse(str));
                 }
             } while (str != null);
         } catch (IOException e){
